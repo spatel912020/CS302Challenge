90541eeea816f38207beb2af21818ed217d75ac1
Shreyank S Patel <spatel91@hydra1.eecs.utk.edu>
2020-03-06 09:39:32 -0500

    Finish Challenge06

---
+++ challenge03/test.cpp
@@ -0,0 +1,18 @@
+// Challenge 06: Palindrome Permutation
+#include <stdio.h>
+#include <ctype.h>
+#include <iostream>
+#include <set>
+#include <sstream>
+
+using namespace std; 
+
+int main(int argc, char *argv[]) {
+	
+	
+	for(int i =0; i < 1000000; i++){
+		cout<<" "<<i<<i<<i<<i<<i<<i<<i<<i<<endl;	
+	}
+	return (0);
+}
+
